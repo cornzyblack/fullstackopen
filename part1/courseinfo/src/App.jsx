742e9799661a274083c21d77f7969d35bc12e1a9
Eniwoke Cornelius <cornzyblack@users.noreply.github.com>
2025-10-03 18:58:26 +0100

    exercise 1.3-1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,49 +1,52 @@
 // Header, Content, and Total
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.courseName}</h1>
   );
 };
 
-const Part = (props) => (<p>{props.name} {props.noExcercises}</p>);
+const Part = (props) => (<p>{props.name} {props.exercises}</p>);
 
 const Content = (props) => {
   return (
   <div>
-      <Part name={props.parts[0].name} noExcercises={props.parts[0].noExcercises}/>
-      <Part name={props.parts[1].name} noExcercises={props.parts[1].noExcercises}/>
-      <Part name={props.parts[2].name} noExcercises={props.parts[2].noExcercises}/>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
   </div>
   );
 };
 
 const Total = (props) => {
   return (
-    <p>Number of exercises: {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    <p>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  let classParts = [{
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
       "name": 'Fundamentals of React',
-      "noExcercises": 10
+      "exercises": 10
     },
     {
       "name": 'Using props to pass data',
-      "noExcercises": 7
+      "exercises": 7
     },
     {
       "name": 'State of a component',
-      "noExcercises": 14
+      "exercises": 14
     }
-  ]
+    ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={classParts} />
-      <Total exercises={[classParts[0].noExcercises, classParts[1].noExcercises, classParts[2].noExcercises] } />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
