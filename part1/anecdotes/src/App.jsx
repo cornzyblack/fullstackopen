506203a76dd4791602b0092ed516cf3b3ae2c847
Eniwoke Cornelius <cornzyblack@users.noreply.github.com>
2025-10-08 15:57:11 +0100

    1.14*: anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 
+const Header = ({ text }) => <h2>{text}</h2>;
+
 const AnecdoteBody = ({ text, noVotes }) => (
   <div>
     <p>{text}</p>
     <p>has {noVotes} votes</p>
   </div>
 );
+
+const AnecdoteVote = ({ text, noVotes }) => {
+  if (noVotes === 0) return null;
+  return <AnecdoteBody text={text} noVotes={noVotes} />
+
+};
 
 const App = () => {
   const anecdotes = [
@@ -17,32 +25,36 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
+  ];
 
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState({0: 0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0})
+  const [selected, setSelected] = useState(0);
+
+  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 });
 
   const getRandomInt = (max) => Math.floor(Math.random() * max);
+  const highestAnecdoteVoteIndex = Number(Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b));
 
   const getRandomAnecdote = () => {
     setSelected(getRandomInt(anecdotes.length));
-    console.log(selected, votes);
   }
   const updateVote = (selected) => {
     const handler = () => {
       const votesCopy = { ...votes };
       const newVoteValue = votesCopy[selected] + 1;
       setVotes({ ...votesCopy, [selected]: newVoteValue });
-      console.log(selected, newVoteValue, votesCopy);
     };
+
     return handler;
   }
 
   return (
     <div>
+      <Header text='Anecdote of the day' />
       <AnecdoteBody text={anecdotes[selected]} noVotes={votes[selected]}/>
       <div><button onClick={getRandomAnecdote}>next anecdote</button></div>
       <div><button onClick={updateVote(selected)}>vote</button></div>
+      <Header text='Anecdote with the most votes' />
+      <AnecdoteVote text={anecdotes[highestAnecdoteVoteIndex]} noVotes={votes[highestAnecdoteVoteIndex]}/>
     </div>
   )
 }
